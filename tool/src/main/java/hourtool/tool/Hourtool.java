82f54a09c60c1968f674462d2ee95a702b601003
Florian Zeidler <zeidler.f@googlemail.com>
2012-12-30 20:17:45 +0100

    Added project for component testing.

---
+++ tool/src/main/java/hourtool/tool/Hourtool.java
@@ -0,0 +1,11 @@
+package hourtool.tool;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: florianzeidler
+ * Date: 30.12.12
+ * Time: 19:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hourtool {
+}
---
+++ tool/src/test/java/hourtool/tool/HourtoolTest.java
@@ -0,0 +1,14 @@
+package hourtool.tool;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: florianzeidler
+ * Date: 30.12.12
+ * Time: 19:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class HourtoolTest {
+
+}
