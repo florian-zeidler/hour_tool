adf8d6e60d13c9c0071423bbaf160069b3273776
Florian Zeidler <zeidler.f@googlemail.com>
2013-01-05 16:21:15 +0100

    Started with the most simple test for creating a timesheet.

--- integration_tests/src/test/java/hourtool/integration/test/TestStartCreatingSimpleTimeSheet.java
+++ integration_tests/src/test/java/hourtool/integration/test/TestStartCreatingSimpleTimeSheet.java
@@ -1,5 +1,8 @@
 package hourtool.integration.test;
 
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
 /**
  * Created with IntelliJ IDEA.
  * User: florianzeidler
@@ -8,4 +11,11 @@ package hourtool.integration.test;
  * To change this template use File | Settings | File Templates.
  */
 public class TestStartCreatingSimpleTimeSheet extends BaseTestFixture {
+
+    @Test
+    public void testCreatingASimpleTimeSheet(){
+        fixture.spinner("start_date_spinner").enterText("01.01.13 16:16");
+        fixture.spinner("stop_date_spinner").enterText("31.01.13 16:16");
+        fixture.button("start_writing_file").click();
+    }
 }
